904d004a312a8a6988a527eec4b9eb9acda1edc6
Murodbek0 <tursunaliyevmurodbek07@gmail.com>
2024-05-19 00:37:00 +0500

    matter

---
+++ js/index.js
@@ -0,0 +1,151 @@
+// 1-misol
+
+
+// alert("butun son berilgan agar, berilgan son musbat bolsa.1 ga oshirilsin aka holda  ozgartrilmasin hosil bolgan sonni ekranga chiqaruvchi programma tuzilsin");
+// let a = + prompt ('a sonni kriting');
+// if (a > 0) {
+//    a++;  
+// } 
+// console.log (a++);
+
+// 2-misol
+
+
+// alert("butun son berilgan agar, berilgan son musbat bolsa.1 ga oshirilsin aka holda 2 kamaytrin ozgartrilmasin hosil bolgan sonni ekranga chiqaruvchi programma tuzilsin");
+// // let a = + prompt ('a sonni kriting');
+// // if (a > 0) { 
+// //    console.log (++a);
+// // }else{
+// //   console.log(-2+a);
+// // };
+
+
+// 3-misol
+
+// alert("butun son berilgan agar, berilgan son musbat bolsa.1 ga oshirilsin aka holda 2 kamaytrin agar 0 ga ten bolsa10 niozlashtirsin.hosil bolgan son ekranga ciqarsilsin");
+
+// let a = + prompt ('a sonni kriting');
+// if (a > 0) { 
+//    console.log (++a);
+// }else {
+//   console.log(a=10);
+  
+// }if(a=0){
+//   console.log(-2 + a);
+// }
+
+// 4-masala
+
+// alert("ushta son berlgan shu sonlar ichida neshta musbat sonberlganini aniqlovchi programa tuzulsin");
+// let musbat = 0;
+// let b = + prompt("son kiriting");
+// if (b > 0) {
+//     musbat++;
+// } 
+// let a = + prompt("son kiriting");
+// if (a > 0) {
+//     musbat++;
+// } 
+// let v = + prompt("son kiriting");
+// if (v > 0) {
+//     musbat++;
+// } 
+// console.log(musbat)
+
+
+// 5-masala
+
+// alert("ushta son berlgan shu sonlar ichida neshta musbatva manfiy sonberlganini aniqlovchi programa tuzulsin")
+// let manfiy = 0;
+// let musbat = 0;
+// let b = + prompt("son kiriting");
+// if (b > 0) {
+//     musbat++;
+// } else {
+//     manfiy++;
+// }
+// let a = + prompt("son kiriting");
+// if (a > 0) {
+//     musbat++;
+// } else {
+//     manfiy++
+// }
+// let v = + prompt("son kiriting");
+// if (v > 0) {
+//     musbat++;
+// } else {
+//     manfiy++;
+// }
+// console.log(manfiy)
+// console.log(musbat)
+
+// 6-masala
+
+// alert("2 ta buton uson berilgan shulardan kattasin aniqlab  programa tuzulsin");
+// let a = Math.max(222, 555);
+// console.log("kattasin: " + a );
+
+
+// 7-masala
+
+// alert("2 ta buton uson berilgan shulardan kichigini aniqlab programa tuzulsin");
+// let a = Math.min(555, 999);
+// console.log("kichigini: " + a );
+
+
+// 8-masala
+
+// alert("2 ta buton uson berilgan shulardan avva kattasin keyin kichigini  aniqlab  programa tuzulsin");
+// let a = Math.max(555, 999);
+// console.log("katta: " + a );
+// let b = Math.min(555, 999);
+// console.log( "kichik :" + b)
+
+// 9-masala
+
+// alert("a va b  haqiqiy sonolar berilga. shu sonlardni shunday ozgartirish kerakki a sono kichik b soni katta bolsin a va b ning qiymatlarni ekranga ciqarilsin");
+
+
+
+
+
+
+
+// 12 masala
+
+// alert("3  son berilgan ushlardan kichigini aniqlab beruvchi programa tuzulsizn ");
+// let a = Math.min(555, 999,222);
+// console.log("eng kichkinasi: " + a );
+
+
+
+// 13-masala
+
+// alert("3  son berilgan ushlardan ortachasini  aniqlab beruvchi programa tuzulsizn ");
+// let s = +prompt('s');
+
+// let d = +prompt('d');
+
+// let C = +prompt('C');
+
+// let m ;
+
+// if ( s > d ) {
+// m = s } ;
+// if (s < C){
+//  m = s} 
+
+//  if ( s < d ) {
+//     m = d } ;
+//     if (d < C){
+//      m = d} console.log(m);
+
+
+// 14-masala
+
+// alert("3 ta son berilgan avval kichigi keyin kattasni topip berivci programa tuzulsin");
+// let a = Math.min(555, 999 ,777);
+// console.log("kichik: " + a );
+// let b = Math.max(555, 999,777);
+// console.log( "katta :" + b);
+
